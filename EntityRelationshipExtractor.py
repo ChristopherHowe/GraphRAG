620a49b8c85641ab331cc37927575292743e5a91
ChristopherHowe <howechristopher5@gmail.com>
2025-05-01 22:46:53 -0700

    simplified and removed a lot of stuff

---
+++ EntityRelationshipExtractor.py
@@ -0,0 +1,49 @@
+from nltk.tokenize import sent_tokenize
+import nltk
+from typing import List
+from models import triplet
+from torch.utils.data import Dataset
+from transformers import pipeline
+
+
+class ER_Extractor:
+    def __init__(self):
+        self.triplet_extractor = pipeline('text2text-generation', model='Babelscape/rebel-large', tokenizer='Babelscape/rebel-large')
+    
+    def extract_ERs(self, sentences: List[str]):
+        tokens = self.triplet_extractor(sentences, return_tensors=True, return_text=False)
+        token_ids = [token["generated_token_ids"] for token in tokens]
+        extracted_text = self.triplet_extractor.tokenizer.batch_decode(token_ids)  # Takes by far the longest out of all the operations here
+        return [triplet for text in extracted_text for triplet in self._extract_triplets(text)]
+
+    def _extract_triplets(self, text) -> List[triplet]:
+        # Source: https://huggingface.co/Babelscape/rebel-large#pipeline-usage
+        triplets = []
+        relation, subject, relation, object_ = '', '', '', ''
+        text = text.strip()
+        current = 'x'
+        for token in text.replace("<s>", "").replace("<pad>", "").replace("</s>", "").split():
+            if token == "<triplet>":
+                current = 't'
+                if relation != '':
+                    triplets.append(triplet(head=subject.strip(), type=relation.strip(), tail=object_.strip()))
+                    relation = ''
+                subject = ''
+            elif token == "<subj>":
+                current = 's'
+                if relation != '':
+                    triplets.append(triplet(head=subject.strip(), type=relation.strip(), tail=object_.strip()))
+                object_ = ''
+            elif token == "<obj>":
+                current = 'o'
+                relation = ''
+            else:
+                if current == 't':
+                    subject += ' ' + token
+                elif current == 's':
+                    object_ += ' ' + token
+                elif current == 'o':
+                    relation += ' ' + token
+        if subject != '' and relation != '' and object_ != '':
+            triplets.append(triplet(head=subject.strip(), type=relation.strip(), tail=object_.strip()))
+        return triplets
--- utils.py
+++ utils.py
@@ -1,48 +1,11 @@
 from nltk.tokenize import sent_tokenize
 import nltk
-from typing import List
-from models import triplet
-from torch.utils.data import Dataset
+
 
 def jupyter_print_paragraph(paragraph: str, line_width: int = 80):
     # Could probably improve this to account for spaces but for now this is okay.
     """Prints a paragraph with wrapping every n characters to make it more readable instead of a single line"""
     print("\n".join([paragraph[i:i+line_width] for i in range(0, len(paragraph), line_width)]))
-
-
-
-
-def extract_triplets(text) -> List[triplet]:
-    # Source: https://huggingface.co/Babelscape/rebel-large#pipeline-usage
-    triplets = []
-    relation, subject, relation, object_ = '', '', '', ''
-    text = text.strip()
-    current = 'x'
-    for token in text.replace("<s>", "").replace("<pad>", "").replace("</s>", "").split():
-        if token == "<triplet>":
-            current = 't'
-            if relation != '':
-                triplets.append(triplet(head=subject.strip(), type=relation.strip(), tail=object_.strip()))
-                relation = ''
-            subject = ''
-        elif token == "<subj>":
-            current = 's'
-            if relation != '':
-                triplets.append(triplet(head=subject.strip(), type=relation.strip(), tail=object_.strip()))
-            object_ = ''
-        elif token == "<obj>":
-            current = 'o'
-            relation = ''
-        else:
-            if current == 't':
-                subject += ' ' + token
-            elif current == 's':
-                object_ += ' ' + token
-            elif current == 'o':
-                relation += ' ' + token
-    if subject != '' and relation != '' and object_ != '':
-        triplets.append(triplet(head=subject.strip(), type=relation.strip(), tail=object_.strip()))
-    return triplets
 
 
 class Sentence_Extractor:
@@ -52,14 +15,13 @@ class Sentence_Extractor:
     def get_sentences(self,text):
         return sent_tokenize(text)
 
-
-class SentenceDataset(Dataset):
-    def __init__(self, sentences):
-        self.sentences = [s for content_sentences in sentences for s in content_sentences]  # Flatten list
+# class SentenceDataset(Dataset):
+#     def __init__(self, sentences):
+#         self.sentences = [s for content_sentences in sentences for s in content_sentences]  # Flatten list
         
-    def __len__(self):
-        return len(self.sentences)
+#     def __len__(self):
+#         return len(self.sentences)
     
-    def __getitem__(self, idx):
-        return self.sentences[idx]
+#     def __getitem__(self, idx):
+#         return self.sentences[idx]
     
